ce15cf9356119cdb40f706a624f349e7996aac73
Adarsh <iadarshh@gmail.com>
2023-02-23 01:16:44 +0530

    Added fe/be code for creating an article also their errors/notices

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,23 @@ class ArticlesController < ApplicationController
     end
 
     def show
-            @article = Article.find(params[:id])
+        @article = Article.find(params[:id])
+    end
+
+    def create
+        # debugger
+        # @article = Article.new(params[:article]) // Wrong way to create a article, use strong params instead.
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        if @article.save
+            flash[:notice] = "Your Article was Created"
+            redirect_to @article
+        else
+            render 'new', status: :unprocessable_entity
+            puts 'error creating article' 
+        end
+    end
+
+    def new
+        @article = Article.new
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
 
   root "articles#index"
 
-  resources :articles, only: [:show, :index]
+  resources :articles, only: [:show, :index, :new, :create]
 end
