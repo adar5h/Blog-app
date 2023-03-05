0eda3f7767b0ed0c3438ca0f326f0f3ef7023403
Adarsh <iadarshh@gmail.com>
2023-03-06 01:25:12 +0530

    add user-signup UI

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
   root "articles#index"
 
   resources :articles # resourcers provides RESTful routes to rails resources
+  get 'signup', to: 'users#new' # new route for users
+  resources :users, except: [:new] #new is already defined
 end
