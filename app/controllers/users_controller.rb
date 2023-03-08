c7790ba4a3123f81bd71cc6ab360e3d537ebf97e
Adarsh <iadarshh@gmail.com>
2023-03-08 16:41:26 +0530

    added BE for user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,21 @@ class UsersController < ApplicationController
     def new 
         @user = User.new
     end
+
+    def create
+        # debugger
+        @user = User.new(user_params)
+        if @user.save
+            flash[:notice] = "Welcome to the blog #{@user.username}, you have successfully created your account."
+            redirect_to articles_path
+        else
+            render 'new', status: :unprocessable_entity
+        end
+    end
+
+    private 
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
 end
