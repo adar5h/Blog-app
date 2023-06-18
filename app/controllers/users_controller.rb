8daaef06f076574051cba5ed07ce38e77ad99940
Adarsh <iadarshh@gmail.com>
2023-06-19 01:28:48 +0530

    added current_user and logged_in? authentication helper methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,14 @@
 class UsersController < ApplicationController
 
+    before_action :set_user , only: [:edit, :update, :show]
     def new 
         @user = User.new
     end
 
     def edit
-        @user = User.find(params[:id])
     end
 
     def update
-        @user = User.find(params[:id])
         if @user.update(user_params)
             flash[:notice] = "User updated successfully"
             redirect_to @user
@@ -19,7 +18,6 @@ class UsersController < ApplicationController
     end
 
     def show
-        @user = User.find(params[:id])
         @articles = @user.articles.paginate(page: params[:page], per_page: 5)
     end
 
@@ -30,6 +28,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id # To keep the user signed in, when they sign up.
             flash[:notice] = "Welcome to the blog #{@user.username}, you have successfully created your account."
             redirect_to articles_path
         else
@@ -41,5 +40,9 @@ class UsersController < ApplicationController
 
     def user_params
         params.require(:user).permit(:username, :email, :password)
+    end
+
+    def set_user 
+        @user = User.find(params[:id])
     end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,12 @@ module ApplicationHelper
         gravatar_url = "https://gravatar.com/avatar/#{hash}?s=#{size}"
         image_tag(gravatar_url, alt: user.username)
     end
+
+    def current_user
+        @current_user ||= User.find(session[:user_id]) if session[:user_id] #Memoization to only hit the DB with query when the value isn't there.
+    end
+
+    def logged_in?
+        !!current_user
+    end
 end
