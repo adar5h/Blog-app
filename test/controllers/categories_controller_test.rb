71ebb16c776c0f8aa404d98c037f5a0d44ca333f
Adarsh <iadarshh@gmail.com>
2023-07-01 01:05:22 +0530

    implemented many-to-many association for article and category

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -1,57 +1,57 @@
-require "test_helper"
+require 'test_helper'
 
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
-  #FunctionalTest
+  # FunctionalTest
   setup do
-    @category = Category.create(name: "Sports")
-    @admin_user = User.create(username: "johndoe", email: "johndoe@example.com", password: "password", admin: true)
+    @category = Category.create(name: 'Sports')
+    @admin_user = User.create(username: 'johndoe', email: 'johndoe@example.com', password: 'password', admin: true)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get categories_url
     assert_response :success
   end
 
-  test "should get new" do
+  test 'should get new' do
     sign_in_as(@admin_user)
     get new_category_url
     assert_response :success
   end
 
-  test "should create category" do
+  test 'should create category' do
     sign_in_as(@admin_user)
-    assert_difference("Category.count", 1) do #added 1, since we wanted the difference to be of 1.
-      post categories_url, params: { category: { name: "Travel" } }
+    assert_difference('Category.count', 1) do #added 1, since we wanted the difference to be of 1.
+      post categories_url, params: { category: { name: 'Travel' } }
     end
 
     assert_redirected_to category_url(Category.last)
   end
 
-  test "should not create category if not admin_user" do
-    assert_no_difference("Category.count") do
-      post categories_url, params: { category: { name: "Travel" } }
+  test 'should not create category if not admin_user' do
+    assert_no_difference('Category.count') do
+      post categories_url, params: { category: { name: 'Travel' } }
     end
 
     assert_redirected_to categories_url
   end
 
-  test "should show category" do
+  test 'should show category' do
     get category_url(@category) # requires the @category to be on the db, therefore in setup we have used create rather than new
     assert_response :success
   end
 
-  # test "should get edit" do
+  # test 'should get edit' do
   #   get edit_category_url(@category)
   #   assert_response :success
   # end
 
-  # test "should update category" do
+  # test 'should update category' do
   #   patch category_url(@category), params: { category: {  } }
   #   assert_redirected_to category_url(@category)
   # end
 
-  # test "should destroy category" do
-  #   assert_difference("Category.count", -1) do
+  # test 'should destroy category' do
+  #   assert_difference('Category.count', -1) do
   #     delete category_url(@category)
   #   end
 
